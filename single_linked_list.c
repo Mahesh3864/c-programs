754f47963a6003c52cc1b1fd5a6c46e796dbfdc8
Mahesh Mathad <mmathad@mmathad-mbp.jnpr.net>
2024-12-14 19:21:20 +0530

    single linked-list functions

--- single_linked_list.c
+++ single_linked_list.c
@@ -1,7 +1,7 @@
 /* All these are only functions */
 
 
-/*Reverse the linked list*/
+/*1. Reverse the linked list*/
 /* Iterative */
 void reverse_list(ST **head)
 {
@@ -16,11 +16,43 @@ void reverse_list(ST **head)
         prev = curr;
         curr = next;
     }
-    return NULL;
+    return;
 }
 
 /* recursive */
 
-ST* reverse_list(ST* head)
+In main, call reverse(NULL,head);
+struct node *head;
+void reverse(struct node *prev, struct node *cur)
 {
+   if(cur){
+      reverse(cur,cur->link);
+      cur->link = prev;
+    }
+    else{
+      head = prev;
+    }
+}
+
+/* 2. Add a nodes in linkedlist in ascending order */
+
+void add_nodes_ascending_order(ST **head, int value)
+{
+    ST *temp = (ST *)malloc(sizeof(ST));
+    temp->data = value;
+    temp->mext = NULL;
+
+    if(*head == NULL || *head->data >= value) {
+        temp->next = *head;
+        *head = temp;
+    } else {
+        ST* temp1 = *head;
+        while(temp1->next!=NULL && temp1->next->data < temp->data)
+            temp1 = temp1->next;
+
+        temp->next = temp1->next;
+        temp1->next = temp;
+    }
+    return;
+}
 
